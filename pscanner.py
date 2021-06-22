e3d97a36a7d6188324bb4882bad08b80c21cb8c0
CyborJ27 <82728673+CyborJ27@users.noreply.github.com>
2021-06-22 19:44:52 +0530

    Update pscanner.py

--- pscanner.py
+++ pscanner.py
@@ -12,7 +12,7 @@ ______
 | |_/ /__  ___ __ _ _ __  _ __   ___ _ __ 
 |  __/ __|/ __/ _` | '_ \| '_ \ / _ \ '__|
 | |  \__ \ (_| (_| | | | | | | |  __/ |   
-\_|  |___/\___\__,_|_| |_|_| |_|\___|_| v1.0
+\_|  |___/\___\__,_|_| |_|_| |_|\___|_| v1.2.0
                                \u001b[0m 
            
                \u001b[3m -  small port scanner by <CyborJ                        
@@ -31,6 +31,8 @@ print("\u001B[34mScanning Target " + ip)
 print("\u001B[34mTime started: "+str(datetime.now()))
 print("-" * 50)
 
+choice=int(input("\u001B[31mEnter 1 for Full scan and 0 for Port range: "))
+
 def scan(ip,port):
     scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     scanner.settimeout(1)
@@ -43,14 +45,29 @@ def scan(ip,port):
     except KeyboardInterrupt:
            print("\nQuit")
            sys.exit()
-    
+if(choice==1):
         
-try:
-       with concurrent.futures.ThreadPoolExecutor (max_workers=100) as executor:
-             for port in range(1,1000):
-                 executor.submit(scan, ip, port)
+ try:
+        with concurrent.futures.ThreadPoolExecutor (max_workers=100) as executor:
+              for port in range(1,65535):
+                  executor.submit(scan, ip, port)
 
     
-except KeyboardInterrupt:
-      print("\u001B[6m\nExit once Again.")
-      sys.exit()
+ except KeyboardInterrupt:
+       print("\u001B[6m\nExit once Again.")
+       sys.exit()
+else:
+ port1 = input("\u001B[31mEnter Start of port range: ")
+ x = int(port1)
+ port2 = input("\u001B[31mEnter end of port  range: ")
+ y = int(port2)
+ print("-" * 50)
+ try:
+        with concurrent.futures.ThreadPoolExecutor (max_workers=100) as executor:
+              for port in range(x,y):
+                  executor.submit(scan, ip, port)
+
+    
+ except KeyboardInterrupt:
+       print("\u001B[6m\nExit once Again.")
+       sys.exit()
